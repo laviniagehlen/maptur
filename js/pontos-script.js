7c2647b81bc9f9ebd0064f5768ef6e5b6034a88a
Lavinia Gehlen <lavigehlen20@gmail.com>
2024-10-09 16:21:56 -0300

    atualizando a estrutura do site e navegação das páginas

--- js/pontos-script.js
+++ js/pontos-script.js
@@ -12,3 +12,21 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 3000); // altere a imagem a cada 3 segundos
 }
+
+// Quando o usuário rolar para baixo 20px a partir do topo da página, mostra o botão
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  const backToTopBtn = document.getElementById("backToTopBtn");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 500) {
+    backToTopBtn.style.display = "block";
+  } else {
+    backToTopBtn.style.display = "none";
+  }
+}
+
+// Quando o usuário clicar no botão, rola a página até o topo
+function topFunction() {
+  document.body.scrollTop = 0; // Para Safari
+  document.documentElement.scrollTop = 0; // Para Chrome, Firefox, IE e Opera
+}
