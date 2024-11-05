f50043ad4c7da461b52fe893cf46e69e7c1fa1a8
Emilly Fernandes <emillyfsa2020@gmail.com>
2024-11-05 16:34:50 -0300

    Alinhando pagina

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 const scrollContainer = document.getElementById('scrollContainer');
 
-    function scrollImages() {
-        scrollContainer.scrollLeft += 1;
+function scrollImages() {
+    scrollContainer.scrollLeft += 1;
 
-        if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
-            scrollContainer.scrollLeft = 0;
-        }
+    if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth - scrollContainer.clientWidth) {
+        scrollContainer.scrollLeft = 0;
     }
+}
 
-    let scrollInterval = setInterval(scrollImages, 20);
+let scrollInterval = setInterval(scrollImages, 20);
 
-    scrollContainer.addEventListener('mouseover', () => {
-        clearInterval(scrollInterval);
-    });
+scrollContainer.addEventListener('mouseover', () => {
+    clearInterval(scrollInterval);
+});
 
-    scrollContainer.addEventListener('mouseout', () => {
-        scrollInterval = setInterval(scrollImages, 20);
-    });
+scrollContainer.addEventListener('mouseout', () => {
+    scrollInterval = setInterval(scrollImages, 20);
+});
+// Seleciona todas as divs com a classe "comment"
+const comments = document.querySelectorAll('.comment');
+
+// Itera sobre cada div selecionada
+comments.forEach((comment, index) => {
+    console.log(`Comentário ${index + 1}:`, comment.textContent);
+    // Exemplo: aplica uma cor de fundo a cada comentário
+    comment.style.backgroundColor = '#f5f5f5'
+});
+
+
